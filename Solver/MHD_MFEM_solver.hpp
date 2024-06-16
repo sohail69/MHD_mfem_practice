35a68e619aff484f892f12f29f99946721b698cc
sohail69 <73966892+sohail69@users.noreply.github.com>
2024-06-16 20:30:56 +0100

    Update MHD_MFEM_solver.hpp

--- Solver/MHD_MFEM_solver.hpp
+++ Solver/MHD_MFEM_solver.hpp
@@ -1,4 +1,16 @@
 /*
  Solver header file
-
+ for an inductionless MHD solver
 */
+
+class MHD_solver : public TimeDependentOperator
+{
+  public:
+    MHD_Solver();
+
+    ~MHD_Solver();
+
+    int GetLogging() const {return logging_;};
+    void SetLogging(int logging){logging_ = logging; };
+
+}
