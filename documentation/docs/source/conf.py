894da2d8c27f2b79bc312b1bddfad152bf7bd0f9
sohail69 <73966892+sohail69@users.noreply.github.com>
2024-06-16 07:07:19 +0100

    Update conf.py

--- documentation/docs/source/conf.py
+++ documentation/docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MHD MFEM'
+copyright = '2021, Sohail'
+author = 'sohail69'
 
 release = '0.1'
 version = '0.1.0'
